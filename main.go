3f156d426c45ffbca69256c174209036546e8d77
Matthew Fisher <matt.fisher@microsoft.com>
2018-03-29 15:22:20 -0700

    code cleanup

--- main.go
+++ main.go
@@ -2,145 +2,42 @@ package main
 
 import (
 	"fmt"
-	
+
+	"github.com/yuin/gluamapper"
 	"github.com/yuin/gopher-lua"
 )
 
 const luaFormulaTypeName = "formula"
 
+// Formula provides instructions and metadata for Draft to install a plugin.
 type Formula struct {
+	// The canonical name of the software.
 	Name string
+	// A (short) description of the software.
 	Description string
+	// The homepage URL for the software.
 	Homepage string
-	SHA256 string
+	// The URL used to download the binary distribution for this version of the formula. The file must be a gzipped tarball (.tar.gz) or a zipfile (.zip) for unpacking.
 	URL string
+	// Additional URLs for this version of the formula.
+	Mirrors []string
+	// To verify the cached download's integrity and security, we verify the SHA-256 hash matches what we've declared in the formula.
+	SHA256 string
+	// Caveats inform the user about any Draft-specific caveats regarding this formula.
+	Caveats string
+	// The version of the software.
 	Version string
-}
-
-func(f *Formula) String() string {
-	return fmt.Sprintf(
-		"%s@%s\n%s\nHomepage: %s\nURL: %s\nsha256: %s",
-		f.Name,
-		f.Version,
-		f.Description,
-		f.Homepage,
-		f.URL,
-		f.SHA256,
-	)
-}
-
-// Registers my Formula type to given L.
-func registerFormulaType(L *lua.LState) {
-	mt := L.NewTypeMetatable(luaFormulaTypeName)
-	L.SetGlobal("formula", mt)
-	// static attributes
-	L.SetField(mt, "new", L.NewFunction(newFormula))
-	// methods
-	L.SetField(mt, "__index", L.SetFuncs(L.NewTable(), formulaMethods))
-}
-
-// Constructor
-func newFormula(L *lua.LState) int {
-	formula := &Formula{}
-	ud := L.NewUserData()
-	ud.Value = formula
-	L.SetMetatable(ud, L.GetTypeMetatable(luaFormulaTypeName))
-	L.Push(ud)
-	return 1
-}
-
-// Checks whether the first lua argument is a *LUserData with *Formula and returns this *Formula.
-func checkFormula(L *lua.LState) *Formula {
-	ud := L.CheckUserData(1)
-	if v, ok := ud.Value.(*Formula); ok {
-		return v
-	}
-	L.ArgError(1, "formula expected")
-	return nil
-}
-
-var formulaMethods = map[string]lua.LGFunction{
-	"name": formulaGetSetName,
-	"description": formulaGetSetDescription,
-	"homepage": formulaGetSetHomepage,
-	"sha256": formulaGetSetSHA256,
-	"url": formulaGetSetURL,
-	"version": formulaGetSetVersion,
-}
-
-// Getter and setter for the Formula#Name
-func formulaGetSetName(L *lua.LState) int {
-	f := checkFormula(L)
-	if L.GetTop() == 2 {
-		f.Name = L.CheckString(2)
-		return 0
-	}
-	L.Push(lua.LString(f.Name))
-	return 1
-}
-
-// Getter and setter for the Formula#Description
-func formulaGetSetDescription(L *lua.LState) int {
-	f := checkFormula(L)
-	if L.GetTop() == 2 {
-		f.Description = L.CheckString(2)
-		return 0
-	}
-	L.Push(lua.LString(f.Description))
-	return 1
-}
-
-// Getter and setter for the Formula#Homepage
-func formulaGetSetHomepage(L *lua.LState) int {
-	f := checkFormula(L)
-	if L.GetTop() == 2 {
-		f.Homepage = L.CheckString(2)
-		return 0
-	}
-	L.Push(lua.LString(f.Homepage))
-	return 1
-}
-
-// Getter and setter for the Formula#SHA256
-func formulaGetSetSHA256(L *lua.LState) int {
-	f := checkFormula(L)
-	if L.GetTop() == 2 {
-		f.SHA256 = L.CheckString(2)
-		return 0
-	}
-	L.Push(lua.LString(f.SHA256))
-	return 1
-}
-
-// Getter and setter for the Formula#URL
-func formulaGetSetURL(L *lua.LState) int {
-	f := checkFormula(L)
-	if L.GetTop() == 2 {
-		f.URL = L.CheckString(2)
-		return 0
-	}
-	L.Push(lua.LString(f.URL))
-	return 1
-}
-
-// Getter and setter for the Formula#Version
-func formulaGetSetVersion(L *lua.LState) int {
-	f := checkFormula(L)
-	if L.GetTop() == 2 {
-		f.Version = L.CheckString(2)
-		return 0
-	}
-	L.Push(lua.LString(f.Version))
-	return 1
 }
 
 func main() {
 	L := lua.NewState()
 	defer L.Close()
-	registerFormulaType(L)
 	if err := L.DoFile("main.lua"); err != nil {
-			panic(err)
+		panic(err)
 	}
-	f := checkFormula(L)
-	fmt.Println(f)
+	var formula Formula
+	if err := gluamapper.Map(L.GetGlobal(luaFormulaTypeName).(*lua.LTable), &formula); err != nil {
+		panic(err)
+	}
+	fmt.Println(formula)
 }
--- main.lua
+++ main.lua
@@ -1,12 +1,10 @@
-f = formula.new{}
+local version = "0.4.0"
 
-version = "0.4.0"
-
-f:name("pack-repo")
-f:description("The Draft pack repository plugin")
-f:homepage("https://github.com/Azure/draft-pack-repo")
-f:version(version)
-f:url("https://azuredraft.blob.core.windows.net/draft/draft-v" .. version .. "-darwin-amd64.tar.gz")
-f:sha256("5caa5cc89d81f193615e3ad55f2c08be59052c3309f7c37d0ed0136d54b82228")
-
-return f
+formula = {
+    name = "pack-repo",
+    description = "The Draft pack repository plugin",
+    homepage = "https://github.com/Azure/draft-pack-repo",
+    version = version,
+    url = "https://azuredraft.blob.core.windows.net/draft/draft-v" .. version .. "-darwin-amd64.tar.gz",
+    sha256 = "5caa5cc89d81f193615e3ad55f2c08be59052c3309f7c37d0ed0136d54b82228"
+}
